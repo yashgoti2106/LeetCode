14fc02afb9968cea4cfe96286ba835e806f09639
Yash Ashok Goti <yash.goti2006@gmail.com>
2026-07-04 09:40:19 +0530

    Time: 0 ms (100%), Space: 43.2 MB (17.44%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        // for(int i = 0; i <sentence.length(); i++ ) {
+        //     for(int j = 'a' )
+        // }
+        for(char c = 'a'; c <= 'z' ;c++) {
+            if(sentence.indexOf(c) == -1) {
+                return false;
+            }
+        } return true;
+    }
+}
